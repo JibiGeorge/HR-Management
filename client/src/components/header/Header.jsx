1c20c6c8a4fd95d98294ce5900d1d98f9ce7b4af
Jibi George <jibiyyan@gmail.com>
2023-01-26 19:57:49 +0530

    Implemented logout button

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { setAdminDetails } from '../../redux/features/adminLogin';
 import './Header.css'
 
 function Header() {
 
+  const dispatch = useDispatch();
+  const naviagate = useNavigate();
+
   const navbarbtn = () => {
     let sidebar = document.querySelector('.sidebar');
     sidebar.classList.toggle("close");
+  }
+
+  // Logout
+  const handleLogout = ()=>{
+    localStorage.removeItem('usertoken');
+    dispatch(setAdminDetails({}))
+    naviagate('/')
   }
   return (
     <div class="home-section d-flex justify-content-between">
@@ -14,7 +27,7 @@ function Header() {
         <span class="text">Drop Down Menu</span>
       </div>
       <div className="logout-btn">
-        <button className='btn btn-primary'>Logout</button>
+        <button className='btn btn-primary' onClick={handleLogout}>Logout</button>
       </div>
     </div>
   )
